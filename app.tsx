638af8783fbb8de28d036aafe4b98589f50d066b
Alexandre Santos <alexandre.santozz@gmail.com>
2021-01-29 17:44:40 +0000

    feat(meta): seo meta tags with search banner

--- app.tsx
+++ app.tsx
@@ -8,15 +8,21 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
       <Head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="A book about writing, testing and maintaining JavaScript and TypeScript web applications using Deno" ></meta>
-        <meta property="og:title" content="Deno Web Development - Alexandre Santos" data-react-helmet="true"></meta>
-        <meta property="og:description" content="A book about writing, testing and maintaining JavaScript and TypeScript web applications using Deno"></meta>
-        <meta property="og:type" content="website"></meta>
-        <meta name="twitter:card" content="summary"></meta>
-        <meta name="twitter:creator" content="Alexandre Portela dos Santos" data-react-helmet="true"></meta>
-        <meta name="twitter:title" content="Deno Web Development - Write, test, maintain JavaScript and TypeScript web applications using Deno"></meta>
-        <meta name="twitter:description" content="A complete guide with step-by-step explanations of Deno’s primitives, using them to build real-word applications."></meta>
         <title>Deno Web Development - Write, test, maintain JavaScript and TypeScript web applications using Deno</title>
+        <meta name="title" content="Deno Web Development - Write, test, maintain JavaScript and TypeScript web applications using Deno"></meta>
+        <meta name="description" content="A book about writing, testing and maintaining JavaScript and TypeScript web applications using Deno"></meta>
+
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://deno-web-development.com/"></meta>
+        <meta property="og:title" content="Deno Web Development - Write, test, maintain JavaScript and TypeScript web applications using Deno"></meta>
+        <meta property="og:description" content="A book about writing, testing and maintaining JavaScript and TypeScript web applications using Deno"></meta>
+        <meta property="og:image" content="/banner-seo.png"></meta>
+
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:url" content="https://deno-web-development.com/"></meta>
+        <meta property="twitter:title" content="Deno Web Development - Write, test, maintain JavaScript and TypeScript web applications using Deno"></meta>
+        <meta property="twitter:description" content="A book about writing, testing and maintaining JavaScript and TypeScript web applications using Deno"></meta>
+        <meta property="twitter:image" content="/banner-seo.png"></meta>
       </Head>
       <Page {...pageProps} />
     </>
