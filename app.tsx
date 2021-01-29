69340978448aa4bb7927d01ddf83af745a369aa7
Alexandre Santos <alexandre.santozz@gmail.com>
2021-01-29 17:21:49 +0000

    feat(meta): seo meta tags

--- app.tsx
+++ app.tsx
@@ -6,8 +6,17 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
   return (
     <>
       <Head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Deno Web Development - Alexandre Santos</title>
+        <meta name="description" content="A book about writing, testing and maintaining JavaScript and TypeScript web applications using Deno" ></meta>
+        <meta property="og:title" content="Deno Web Development - Alexandre Santos" data-react-helmet="true"></meta>
+        <meta property="og:description" content="A book about writing, testing and maintaining JavaScript and TypeScript web applications using Deno"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:creator" content="Alexandre Portela dos Santos" data-react-helmet="true"></meta>
+        <meta name="twitter:title" content="Deno Web Development - Write, test, maintain JavaScript and TypeScript web applications using Deno"></meta>
+        <meta name="twitter:description" content="A complete guide with step-by-step explanations of Deno’s primitives, using them to build real-word applications."></meta>
+        <title>Deno Web Development - Write, test, maintain JavaScript and TypeScript web applications using Deno</title>
       </Head>
       <Page {...pageProps} />
     </>
