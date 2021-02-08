b9dfe9c78f60b08b856fb4ea6b2d84b657ab80b3
Felipe Schmitt <492108+fsschmitt@users.noreply.github.com>
2021-02-08 20:58:44 +0000

    fix(banner): update banner with lighter jpg (#1)

--- app.tsx
+++ app.tsx
@@ -16,17 +16,17 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
         <meta property="og:url" content="https://deno-web-development.com/"></meta>
         <meta property="og:title" content="Deno Web Development - Write, test, maintain and deploy JavaScript and TypeScript web applications using Deno"></meta>
         <meta property="og:description" content="A complete guide with step-by-step explanations of Deno's primitives, using them to build real-word applications. A guided process on learning Deno and its features while a complete web application, from hello world, to testing, adding users and deploying."></meta>
-        <meta property="og:image" content="https://deno-web-development.com/banner-seo.png"></meta>
+        <meta property="og:image" content="https://deno-web-development.com/banner-seo.jpg"></meta>
         <meta property="og:image:width" content="1200"></meta>
         <meta property="og:image:height" content="600"></meta>
-        <meta property="og:image:secure_url" content="https://deno-web-development.com/banner-seo.png"></meta>
+        <meta property="og:image:secure_url" content="https://deno-web-development.com/banner-seo.jpg"></meta>
 
         <meta property="twitter:card" content="summary_large_image"></meta>
         <meta property="twitter:url" content="https://deno-web-development.com/"></meta>
         <meta property="twitter:title" content="Deno Web Development - Write, test, maintain and deploy JavaScript and TypeScript web applications using Deno"></meta>
         <meta property="twitter:description" content="A complete guide with step-by-step explanations of Deno's primitives, using them to build real-word applications. A guided process on learning Deno and its features while a complete web application, from hello world, to testing, adding users and deploying."></meta>
-        <meta property="twitter:image" content="https://deno-web-development.com/banner-seo.png"></meta>
-        <link rel="image_src" href="https://deno-web-development.com/banner-seo.png" />
+        <meta property="twitter:image" content="https://deno-web-development.com/banner-seo.jpg"></meta>
+        <link rel="image_src" href="https://deno-web-development.com/banner-seo.jpg" />
       </Head>
       <Scripts>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-2ETE5MRE3Y"></script>
